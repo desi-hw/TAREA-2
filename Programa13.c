0f289fc49f2a9aabeb95def1f97af997758581b1
desi-hw <desireewasszabo@gmail.com>
2025-11-11 20:20:05 -0600

    Create Programa13.c

---
+++ Programa13.c
@@ -0,0 +1,18 @@
+/*Huitrón Was-Szabo Elizabeth Desireé
+Problemas con Arreglos Ejercicio 13*/
+#include<stdio.h>
+int main()
+{
+  int num[1000], num2, i=0, suma=0;
+  printf("¿Cuantos numeros quieres sumar? \n");
+  scanf("%d", &num2);
+  while(i<num2)
+  {
+    printf("Ingrese un numero: ");
+    scanf("%d", &num[i]);
+    suma = suma + num[i];
+    i++;
+  }
+  printf("El resultado de la suma es: %d", suma);
+  return 0;
+}
