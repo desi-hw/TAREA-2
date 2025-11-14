7927d9091ed12c26b14c8154c3bb8a25d6770a58
desi-hw <desireewasszabo@gmail.com>
2025-11-14 12:39:48 -0600

    Create Programa16.c

---
+++ Programa16.c
@@ -0,0 +1,14 @@
+/*Huitrón Was-Szabo Elizabeth Desireé
+Problemas con Arreglos Ejercicio 16*/
+#include<stdio.h>
+int main()
+{
+  int i=1;
+  for(i; i<=100; i++)
+  {
+    int arreglo[i];
+    if(i % 2 == 0)
+      printf("%d \n", i);
+  }
+  return 0;
+}
