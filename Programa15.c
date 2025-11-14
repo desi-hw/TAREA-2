cf709f4fef6f32566f11a8d6318fb0a2b93ff850
desi-hw <desireewasszabo@gmail.com>
2025-11-14 12:25:18 -0600

    Update Programa15.c

--- Programa15.c
+++ Programa15.c
@@ -1,11 +1,25 @@
 /*Huitrón Was-Szabo Elizabeth Desireé
 Problemas con Arreglos Ejercicio 15*/
-include<stdio.h>
+#include<stdio.h>
 int main()
 {
-  int num;
-  printf("¿Cuantos numeros quieres meter?");
+  int num, i;
+  printf("¿Cuantos numeros quieres meter? \n");
   scanf("%d", &num);
   int arreglo[num];
-  
+  for(i=0; i<num; i++)
+  {
+    
+    if(arreglo[i]>0)
+      printf("El numero es postivo \");
+    else
+    {
+      if(arreglo[i]<0)
+        printf("El numero es negativo \");
+      else
+        printf("El numero es nulo \"); 
+    }
+    i++;
+  }
+  return 0;
 }
