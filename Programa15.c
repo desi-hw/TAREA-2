ff4c4b3e6841c0f1a4b92d4aa4bc915625c9056e
desi-hw <desireewasszabo@gmail.com>
2025-11-14 10:42:22 -0600

    Create Programa15.c

---
+++ Programa15.c
@@ -0,0 +1,11 @@
+/*Huitrón Was-Szabo Elizabeth Desireé
+Problemas con Arreglos Ejercicio 15*/
+include<stdio.h>
+int main()
+{
+  int num;
+  printf("¿Cuantos numeros quieres meter?");
+  scanf("%d", &num);
+  int arreglo[num];
+  
+}
